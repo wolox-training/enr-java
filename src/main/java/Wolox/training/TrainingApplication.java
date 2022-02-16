7b9e4204e7bf56f9b4cb180a9fba04124eca7610
Elias <ravarottoelias@gmail.com>
2022-02-16 15:17:34 -0300

    Base proyect was created

--- src/main/java/Wolox/training/TrainingApplication.java
+++ src/main/java/Wolox/training/TrainingApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TrainingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("bla la");
 		SpringApplication.run(TrainingApplication.class, args);
 	}
 
