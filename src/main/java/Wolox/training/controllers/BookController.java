6b3af55e3b92807232b66ed643809f43a9f21b54
Elias <ravarottoelias@gmail.com>
2022-02-21 14:06:05 -0300

    Documentation was added

--- src/main/java/Wolox/training/controllers/BookController.java
+++ src/main/java/Wolox/training/controllers/BookController.java
@@ -12,6 +12,12 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/api/books")
 public class BookController {
+    /***
+     *
+     * @param name received from the query
+     * @param model used to set attributes to the view
+     * @return a String which is the name of template
+     */
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
@@ -21,18 +27,37 @@ public class BookController {
     @Autowired
     private BookRepository bookRepository;
 
+    /***
+     * This method creates a book
+     *
+     * @param book: contain the book's data
+     * @return Book created
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Book create(@RequestBody Book book) {
         return bookRepository.save(book);
     }
 
+    /***
+     *This method find a book
+     *
+     * @param id: identifier of the book
+     * @return a Book instance
+     */
     @GetMapping("/{id}")
     public Book findOne(@PathVariable Long id) {
         return bookRepository.findById(id)
                 .orElseThrow(BookNotFoundException::new);
     }
 
+    /***
+     * This method updates a book
+     *
+     * @param book: contain the book's data
+     * @param id: identifier of the book to update
+     * @return Book updated.
+     */
     @PutMapping("/{id}")
     public Book updateBook(@RequestBody Book book, @PathVariable Long id) {
         if (book.getId() != id) {
@@ -43,6 +68,11 @@ public class BookController {
         return bookRepository.save(book);
     }
 
+    /***
+     * This method remove a book
+     *
+     * @param id: identifier of the book to delete
+     */
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         bookRepository.findById(id)
--- src/main/java/Wolox/training/repositories/BookRepository.java
+++ src/main/java/Wolox/training/repositories/BookRepository.java
@@ -5,7 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/***
+ * This is a repository to work on book operations
+ */
 public interface BookRepository extends JpaRepository<Book, Long> {
 
+    /***
+     * This method finds a book by author
+     *
+     * @param author
+     * @return a book
+     */
     Optional<Book> findByAuthor(String author);
 }
