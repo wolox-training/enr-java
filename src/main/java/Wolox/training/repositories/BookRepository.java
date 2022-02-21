664881daf6b653e7526caaa23b291982e07f30b8
Elias <ravarottoelias@gmail.com>
2022-02-21 10:57:51 -0300

    fix: method findByAuthor.

--- src/main/java/Wolox/training/repositories/BookRepository.java
+++ src/main/java/Wolox/training/repositories/BookRepository.java
@@ -2,9 +2,9 @@ package Wolox.training.repositories;
 
 import Wolox.training.models.Book;
 import org.springframework.data.repository.Repository;
-import java.util.List;
+import java.util.Optional;
 
 public interface BookRepository extends Repository<Book, Long> {
 
-    List<Book> findByAuthor(String author);
+    Optional<Book> findByAuthor(String author);
 }
