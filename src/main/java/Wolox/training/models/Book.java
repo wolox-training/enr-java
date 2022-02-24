13ba13a6e26a548e3f6b14c2eb628c6aa1763996
eliasrvt <ravarottoelias@gmail.com>
2022-02-24 14:50:31 -0300

    Update src/main/java/Wolox/training/models/Book.java
    Co-authored-by: Juan David Bermudez Celedon <juandavid.bermudez@wolox.co>

--- src/main/java/Wolox/training/models/Book.java
+++ src/main/java/Wolox/training/models/Book.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 @Entity
 public class Book {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
 
     @Column(nullable = false, unique = true)
