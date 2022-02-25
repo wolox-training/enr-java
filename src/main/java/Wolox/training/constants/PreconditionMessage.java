4db02cef1ce74aaca05b99ae55c4c72967fdd4d8
Elias <ravarottoelias@gmail.com>
2022-02-25 13:00:50 -0300

    Preconditions and messages were added to user and book model

---
+++ src/main/java/Wolox/training/constants/PreconditionMessage.java
@@ -0,0 +1,7 @@
+package wolox.training.constants;
+
+public class PreconditionMessage {
+    public static final String NOT_NULL_OR_EMPTY = "The provided value must not be empty or null";
+    public static final String YEAR_IS_LESS = "Year must be greater than 0";
+    public static final String YEAR_IS_GREATER = "Year must be less than or equal to the current year";
+}
